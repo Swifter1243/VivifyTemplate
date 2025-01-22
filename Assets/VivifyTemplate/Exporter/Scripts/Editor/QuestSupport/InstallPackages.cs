876a269ebe48e0d5e41104b851eed786a3d635f1
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2025-01-21 22:53:06 -0500

    InstallPackages

---
+++ Assets/VivifyTemplate/Exporter/Scripts/Editor/QuestSupport/InstallPackages.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+public static class InstallPackages
+{
+    static Stack<string> Requests = new Stack<string>();
+    static AddRequest InstallingRequest;
+
+    public static void Setup()
+    {
+        EditorGUILayout.LabelField("hi");
+        if (GUILayout.Button("Install"))
+        {
+            Requests.Push("com.unity.xr.oculus");
+            Requests.Push("com.unity.xr.openxr");
+            Requests.Push("com.unity.xr.management");
+            Requests.Push("com.unity.inputsystem");
+            EditorApplication.update += Progress;
+            Progress();
+        }
+    }
+    
+    private static void Progress()
+    {
+        if (InstallingRequest == null)
+        {
+            var request = Requests.Pop();
+            Debug.Log($"Installing {request}");
+            InstallingRequest = Client.Add(request);
+        }
+        Debug.Log(InstallingRequest.Status);
+        if (InstallingRequest.IsCompleted)
+        {
+            if (InstallingRequest.Status == StatusCode.Success)
+            {
+                Debug.Log("Installed: " + InstallingRequest.Result.packageId);
+                InstallingRequest = null;
+            }
+            else if (InstallingRequest.Status >= StatusCode.Failure)
+            {
+                Debug.Log(InstallingRequest.Error.message);
+                InstallingRequest = null;
+            }
+            Progress();
+        }
+    }
+}
