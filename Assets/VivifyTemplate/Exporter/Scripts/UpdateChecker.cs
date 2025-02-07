7b80c8448c2980ad351926ed405b6c2258a39357
Swifter <swifter1243@gmail.com>
2025-02-06 20:00:25 -0500

    GetGitHubVersion stub

--- Assets/VivifyTemplate/Exporter/Scripts/UpdateChecker.cs
+++ Assets/VivifyTemplate/Exporter/Scripts/UpdateChecker.cs
@@ -1,9 +1,25 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 namespace VivifyTemplate.Exporter.Scripts
 {
 	[InitializeOnLoad]
 	public static class UpdateChecker
 	{
+		private static Version _templateVersion = new Version("1.0.0");
 
+		static UpdateChecker()
+		{
+
+		}
+
+		private static void CheckForUpdates()
+		{
+
+		}
+
+		private static Version GetGitHubVersion()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
