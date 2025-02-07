67c0f4a0952609681ccbbb994d0214cb449aad16
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2025-02-06 20:38:57 -0500

    Fetch version

--- Assets/VivifyTemplate/Exporter/Scripts/UpdateChecker.cs
+++ Assets/VivifyTemplate/Exporter/Scripts/UpdateChecker.cs
@@ -1,21 +1,24 @@
-﻿using System;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
+
 namespace VivifyTemplate.Exporter.Scripts
 {
 	[InitializeOnLoad]
 	public static class UpdateChecker
 	{
-		private readonly static Version TemplateVersion = new Version("1.0.0");
-		private readonly static string InitializeBool = "UpdateCheckerInitialized";
+		private static readonly Version TemplateVersion = new Version("1.0.0");
+		private static readonly HttpClient Client = new HttpClient();
+		private const string InitializeBool = "UpdateCheckerInitialized";
+		private const string Repo = "Swifter1243/Remapper";
 
 		static UpdateChecker()
 		{
-			if (SessionState.GetBool(InitializeBool, false))
-			{
-				return;
-			}
-
+			Client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", $"Swifter1243/VivifyTemplate/{TemplateVersion}");
 			SessionState.SetBool(InitializeBool, true);
 			CheckForUpdates();
 		}
@@ -33,7 +36,28 @@ namespace VivifyTemplate.Exporter.Scripts
 
 		private static Version GetGitHubVersion()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				Task<HttpResponseMessage> response = Client.GetAsync($"https://api.github.com/repos/{Repo}/tags");
+				response.Wait();
+				response.Result.EnsureSuccessStatusCode();
+				Task<string> responseBody = response.Result.Content.ReadAsStringAsync();
+				responseBody.Wait();
+				GithubVersion[] versions = Newtonsoft.Json.JsonConvert.DeserializeObject<GithubVersion[]>(responseBody.Result);
+				return Version.Parse(versions[0].name);
+			}
+			catch (HttpRequestException e)
+			{
+				Debug.LogException(e);
+			}
+			
+			throw new ApplicationException("Failed to get latest version from GitHub");
 		}
+
+		[Serializable]
+		private class GithubVersion
+		{
+			public string name;
+		} 
 	}
 }
